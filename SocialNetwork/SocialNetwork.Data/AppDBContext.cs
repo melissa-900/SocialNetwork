ec007937d3a0c4a93aff1b2ba61ee9ca3dd8fd5a
nazar <nazar.heiko@gmail.com>
2025-03-19 16:55:58 +0000

    Set up SQL Database

---
+++ SocialNetwork/SocialNetwork.Data/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Data.Models;
+
+namespace SocialNetwork.Data
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasMany(u => u.Posts).WithOne(p => p.User).HasForeignKey(p => p.UserId);
+        }
+    }
+}
---
+++ SocialNetwork/SocialNetwork.Data/Helpers/DbInitializer.cs
@@ -0,0 +1,48 @@
+﻿using SocialNetwork.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Data.Helpers
+{
+   public static class DbInitializer
+    {
+
+        public static async Task SeedAsync(AppDBContext appDBContext)
+        {
+            if(!appDBContext.Users.Any() && !appDBContext.Posts.Any())
+            {
+                var newUser = new User()
+                {
+                    FullName = "Lorem ipsum",
+                    ProfilePictureUrl = "#"
+                };
+                await appDBContext.Users.AddAsync(newUser);
+                await appDBContext.SaveChangesAsync();
+                var newPostWithoutImage = new Post()
+                {
+                    Content = "First post, wich was loaded from database and was created by test user",
+                    ImageUrl = " ",
+                    NrOfReports =0,
+                    DateCreated = DateTime.UtcNow,
+                    DateUpdeted = DateTime.UtcNow,
+                    UserId = newUser.Id
+                };
+                var newPostWithImage = new Post()
+                {
+                    Content = "First post, wich was loaded from database and was created by test user",
+                    ImageUrl = "https://i.etsystatic.com/28810262/r/il/2fc5e0/5785166966/il_1588xN.5785166966_nvy4.jpg",
+                    NrOfReports = 0,
+                    DateCreated = DateTime.UtcNow,
+                    DateUpdeted = DateTime.UtcNow,
+                    UserId = newUser.Id
+                };
+
+                await appDBContext.Posts.AddRangeAsync(newPostWithoutImage, newPostWithImage);
+                await appDBContext.SaveChangesAsync();
+            }
+        }
+    }
+}
