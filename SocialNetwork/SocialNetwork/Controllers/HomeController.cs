b5cfbffbb9700873908b4201dfcf60a9d5f8acda
melissa-900 <rud.melissa@lll.kpi.ua>
2025-04-03 11:21:17 +0300

    .

--- SocialNetwork/SocialNetwork/Controllers/HomeController.cs
+++ SocialNetwork/SocialNetwork/Controllers/HomeController.cs
@@ -5,8 +5,8 @@ namespace SocialNetwork.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
 
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
