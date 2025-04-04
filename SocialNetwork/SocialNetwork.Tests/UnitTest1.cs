afbd3f2f7bd6475c3e5025ac154d159c7c423710
melissa-900 <rud.melissa@lll.kpi.ua>
2025-04-04 12:57:59 +0300

    Added Unit test for valid text-only Post.

---
+++ SocialNetwork/SocialNetwork.Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+using Moq;
+using SocialNetwork.Controllers;
+using SocialNetwork.Data;
+using SocialNetwork.Data.Models;
+using SocialNetwork.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SocialNetwork.Tests
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController _controller;
+        private readonly Mock<ILogger<HomeController>> _mockLogger;
+        private readonly AppDBContext _context;
+
+        public HomeControllerTests()
+        {
+            // Create In-Memory DB for tests
+            var options = new DbContextOptionsBuilder<AppDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase").Options;
+
+            // Create context
+            _context = new AppDBContext(options);
+
+            // Mock Logger
+            _mockLogger = new Mock<ILogger<HomeController>>();
+
+            // Create controller with InMemory context
+            _controller = new HomeController(_mockLogger.Object, _context);
+        }
+
+        // CrestePost TESTS
+
+        [Fact]
+        public async Task CreatePost_ShouldAddPostToDatabase()
+        {
+            // Arrange
+            var postVM = new PostVM
+            {
+                Content = "Hello World!",
+                Image = null
+            };
+
+            // Act
+            var result = await _controller.CreatePost(postVM);
+
+            // Assert
+            var postCount = await _context.Posts.CountAsync();
+            Assert.Equal(1, postCount); // Check if post was added
+            Assert.IsType<RedirectToActionResult>(result); // Ensure it redirects to Index page
+        }
+    }
+}
